2cb9c72226c4e05843677801b421b29cdd158fb0
Wako Anindo <wakoanindo@gmail.com>
2025-01-05 23:42:02 +0000

    Implement isSpotAvailable and check IF the index given to placeMarker is valid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,18 +2,32 @@ const Gameboard = (function() {
     let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
     function placeMarker(index, marker) {
-        // method logic
+        // Check IF the index is valid (within the bounds of the board array)
+        if(index >=0 && index <= 8) {
+            // put isSpotAvailable here?
+            if (isSpotAvailable(index)) {
+                // Place marker in this spot
+            }
+        }
+
+        // Check if the spot is available (you can use isSpotAvailable for this if it's implemented).
+
+        // If valid && available, place the marker at the specified index
     }
 
     function getBoardState() {
         return board;
     }
 
+    function isSpotAvailable(index) {
+        return board[index] === ' ';
+    }
+
     return {
         // methods
         getBoardState,
         placeMarker,
-        // isSpotAvailable
+        isSpotAvailable
     }
 })();
 
